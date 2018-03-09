2741d9c762213933e98fc38f180b643d8c23780f
InesAtm <ineskatam@gmail.com>
2018-03-09 16:43:38 +0100

    add of annotations

--- src/main/java/AtmaniEspinasse/TpMorphia/Article.java
+++ src/main/java/AtmaniEspinasse/TpMorphia/Article.java
@@ -3,16 +3,30 @@ package AtmaniEspinasse.TpMorphia;
 import java.util.Collection;
 
 import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Embedded;
 import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
+import org.mongodb.morphia.annotations.Property;
 
 @Entity
 public class Article {
+	@Id
+	@Property("id")
 	private ObjectId id;
 	private String name;
+	@Embedded
 	private Collection<Person> persons;
 
 	public Article() {
 
+	}
+	
+	public ObjectId getId() {
+		return id;
+	}
+
+	public void setId(ObjectId id) {
+		this.id = id;
 	}
 
 	public String getName() {
--- src/main/java/AtmaniEspinasse/TpMorphia/Person.java
+++ src/main/java/AtmaniEspinasse/TpMorphia/Person.java
@@ -3,16 +3,30 @@ package AtmaniEspinasse.TpMorphia;
 import java.util.Collection;
 
 import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Embedded;
 import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
+import org.mongodb.morphia.annotations.Property;
 
 @Entity
 public class Person {
+	@Id
+	@Property("id")
 	private ObjectId id;
 	private String name;
+	@Embedded
 	private Collection<Address> adresses;
 
 	public Person() {
 
+	}
+	
+	public ObjectId getId() {
+		return id;
+	}
+
+	public void setId(ObjectId id) {
+		this.id = id;
 	}
 
 	public String getName() {
